4f8fb511717ed337f54aa193d30d4bc07e91c953
KTsaneff <78788769+KTsaneff@users.noreply.github.com>
2024-11-06 15:15:20 +0200

    Initial commit

---
+++ CinemaApp.Services.Data/Interfaces/ICinemaService.cs
@@ -0,0 +1,17 @@
+﻿namespace CinemaApp.Services.Data.Interfaces
+{
+    using Web.ViewModels.Cinema;
+
+    public interface ICinemaService
+    {
+        Task<IEnumerable<CinemaIndexViewModel>> IndexGetAllOrderedByLocationAsync();
+
+        Task AddCinemaAsync(AddCinemaFormModel model);
+
+        Task<CinemaDetailsViewModel?> GetCinemaDetailsByIdAsync(Guid id);
+
+        Task<EditCinemaFormModel?> GetCinemaForEditByIdAsync(Guid id);
+
+        Task<bool> EditCinemaAsync(EditCinemaFormModel model);
+    }
+}
